75f4386ab2497780d63d70d46670c5f568c97b93
Kirsten231296 <125568773+KIRSTEN-23@users.noreply.github.com>
2026-06-15 04:29:09 +0200

    final buyer side change: api route change for browse page

--- backend/features/listings/buyerListingController.js
+++ backend/features/listings/buyerListingController.js
@@ -19,16 +19,16 @@ as it only serves as guidelines for development and is not needed in the final c
 USE THE FOLLOWING FUNCTION AND ROUTE SIGNATURES FOR THIS CONTROLLER
 
 getAllAvailablePublishedListings()
-GET /api/public/listings
+GET /api/public/browse
 
 getListingById()
-GET /api/public/listings/:id
+GET /api/public/browse/:id
 
 searchListings()
-GET /api/public/listings/search
+GET /api/public/browse/search
 
 getFeaturedListings()
-GET /api/public/listings/featured
+GET /api/public/browse/featured
 
 */
 
