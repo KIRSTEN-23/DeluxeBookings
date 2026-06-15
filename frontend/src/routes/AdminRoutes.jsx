b6e282112e45ba3b1a57c2c50e0761f6d6d33ae5
Kirsten231296 <125568773+KIRSTEN-23@users.noreply.github.com>
2026-06-15 09:10:31 +0200

    admin files

--- frontend/src/routes/AdminRoutes.jsx
+++ frontend/src/routes/AdminRoutes.jsx
@@ -1,17 +1,17 @@
-import { Route } from "react-router-dom";
+// import { Route } from "react-router-dom";
 
-import ProtectedRoutes from "./ProtectedRoutes";
+// import ProtectedRoutes from "./ProtectedRoutes";
 
-import AdminListings from "../pages/userAccounts/AdminDashboard";
-import SellerAdminListingDetails from "../pages/listings/SellerAdminListingDetails";
+// import AdminListings from "../pages/userAccounts/AdminDashboard";
+// import SellerAdminListingDetails from "../pages/listings/SellerAdminListingDetails";
 
-export default function AdminRoutes() {
-  return (
-    <>
-      {/* <Route element={<ProtectedRoutes minLevel={3} />}> */}
-        <Route path="/admin/listings" element={<AdminListings />} />
-        <Route path="/admin/listings/:id" element={<SellerAdminListingDetails role="admin" />} />
-      {/* </Route> */}
-    </>
-  );
-}
+// export default function AdminRoutes() {
+//   return (
+//     <>
+//       {/* <Route element={<ProtectedRoutes minLevel={3} />}> */}
+//         <Route path="/admin/listings" element={<AdminListings />} />
+//         <Route path="/admin/listings/:id" element={<SellerAdminListingDetails role="admin" />} />
+//       {/* </Route> */}
+//     </>
+//   );
+// }
